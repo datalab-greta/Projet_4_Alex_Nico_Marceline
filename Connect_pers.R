f3e31fbcce06fe51a3268a8dce1b0722a50cf6a1
Alexandre <51398853+Gerikk@users.noreply.github.com>
2019-10-22 16:09:44 +0200

    Nouvelle version du script
    Dataviz liées aux messages:
    Séparation des messages par année uniquement
    Cours avec messages discussion/question

--- Connect_pers.R
+++ Connect_pers.R
@@ -1,6 +1,7 @@
-#install.packages("mongolite")
+library(tidyverse)
 library(config)
 library(RPostgreSQL)
+library(ggplot2)
 #Définition du chemin du dossier config.rml (à créer avant)
 conf <- config::get("Postgre", file="~/Documents/Projet_4/.config.yml", use_parent = FALSE)
 
@@ -16,6 +17,56 @@ drv <- dbDriver("PostgreSQL")
 mydb<- dbConnect(drv, user=user1, password=password1, dbname=Dt, host=host)
 DBI::dbListTables(mydb)
 
-usernames10 = DBI::fetch(dbSendQuery(mydb, "select * from test.all_moocs_alex limit 10"), n=-1)
+selection= DBI::fetch(dbSendQuery(mydb, "SELECT * FROM mooc_grp_man.nicolas2 ORDER BY date"), n=-1)
+
 #-----------Pour la déconnection!-----------------------
 dbDisconnect(mydb)
+
+
+#----------Dataviz-----------------------------------
+#Creation of a color palette with different sizes for plots
+ColorschemeBG<-grDevices::colorRampPalette(colorRamps::blue2green(30))
+Colours<-ColorschemeBG(100)
+Col_L<-ColorschemeBG(300)
+Col_S<-ColorschemeBG(6)
+fillBG<-ggplot2::scale_fill_manual(values=Colours)
+L_fillBG<-ggplot2::scale_fill_manual(values=Col_L)
+S_fillBG<-ggplot2::scale_fill_manual(values=Col_S)
+coulBG<-ggplot2::scale_color_manual(values=Colours)
+L_coulBG<-ggplot2::scale_color_manual(values=Col_L)
+S_coulBG<-ggplot2::scale_color_manual(values=Col_S)
+
+#-------Nombre de messages par année-------
+Dates<-selection %>%
+  dplyr::group_by(id, course_id, date, body, title, courseware_title, thread_type, username) %>%
+  dplyr::summarise(Nombre = n())
+Dates$date<-substr(Dates$date, 0, 4)
+
+
+plot1<-ggplot2::ggplot(Dates)+ geom_col(aes(x= date, y=Nombre, fill=date)) + S_fillBG + ggplot2::labs(title ="Nombre de messages de 2014 à 2019", x = "Dates", y="Nombre de messages")
+plot1
+
+#----Dataframes des plots
+
+cours_questions<-selection %>%
+  dplyr::group_by(course_id, thread_type) %>%
+  dplyr::summarise(Nombre = n()) %>%
+  filter(thread_type == 'question')
+cours_questions$course_id <- factor(cours_questions$course_id, levels = cours_questions$course_id[order(cours_questions$Nombre)])
+
+cours_discussions<-selection %>%
+  dplyr::group_by(course_id, thread_type) %>%
+  dplyr::summarise(Nombre = n()) %>%
+  filter(thread_type == 'discussion')
+cours_discussions$course_id <- factor(cours_discussions$course_id, levels = cours_discussions$course_id[order(cours_discussions$Nombre)])
+
+
+#---------Plot du top 20 des questions----------
+
+Questions<-ggplot2::ggplot(cours_questions[1:20,]) + geom_col(aes(x = course_id, y= Nombre, fill=Nombre))+scale_fill_gradient(low = "yellow", high = "orange")
+Questions + coord_polar() + S_coulBG + labs(title = "Top 20 de questions par cours", x='Cours', y="Nombre d'occurences")
+
+#---------Plot du top 20 des discussions----------
+
+Discussions<-ggplot2::ggplot(cours_discussions[1:20,]) + geom_col(aes(x = course_id, y= Nombre, fill=Nombre))
+Discussions + S_coulBG + labs(title = "Top 20 de discussions par cours", x='Cours', y="Nombre d'occurences") + coord_flip()+scale_fill_gradient(low = "#007F7F", high = "green")
